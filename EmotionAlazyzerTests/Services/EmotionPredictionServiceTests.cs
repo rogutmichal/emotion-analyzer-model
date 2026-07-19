f90f3a2f3163b08440b3f5cc7e326848229e1fed
rogutmichal <rogutmichal00@gmail.com>
2026-07-19 20:04:48 +0200

    add two testes

---
+++ EmotionAlazyzerTests/Services/EmotionPredictionServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using EmotionAnalyzerML.Services;
+using Microsoft.ML;
+
+
+namespace EmotionAnalyzerTests.Services
+{
+    public class EmotionPredictionServiceTests
+    {
+        // Tests if the service rejects empty input text
+        [Fact]
+        public void Predict_ShouldThrowException_WhenTextIsEmpty()
+        {
+            var context = new MLContext();
+
+
+            var loadedModel =
+                new LoadedModelService();
+
+
+
+            var service =
+                new EmotionPredictionService(
+                    context,
+                    loadedModel);
+
+
+
+
+            Assert.Throws<ArgumentException>(
+                () =>
+                    service.Predict("")
+            );
+        }
+
+        // Tests if prediction cannot be performed when the ML model has not been loaded
+        [Fact]
+        public void Predict_ShouldThrowException_WhenModelIsNotLoaded()
+        {
+
+            var context = new MLContext();
+
+
+            var loadedModel =  new LoadedModelService();
+
+
+
+            var service = new EmotionPredictionService(context,loadedModel);
+
+
+
+            Assert.Throws<InvalidOperationException>(() =>service.Predict("I love this movie")
+            );
+        }
+    }
+}
